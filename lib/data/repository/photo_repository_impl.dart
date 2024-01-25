39e5639180211dbc0e6d2d0b96b50aa1f972c8e0
Marzbani <hamied.reza.m@gmail.com>
2024-01-25 19:59:46 +0330

    [DEV] the repository has been implemented

--- lib/data/repository/photo_repository_impl.dart
+++ lib/data/repository/photo_repository_impl.dart
@@ -1,31 +1,32 @@
 import 'package:communere/app/base/api_result.dart';
-import 'package:communere/data/data_source/remote/photos_remote_data_source.dart';
+import 'package:communere/domain/data_source/photo_data_source.dart';
 import 'package:communere/domain/entities/photo/photo.dart';
 import 'package:communere/domain/entities/search/search_criteria.dart';
 
 import '../../domain/repository/photos_repository.dart';
 
-class PhotoRepositoryImpl extends PhotosRepository{
+class PhotoRepositoryImpl extends PhotosRepository {
+  final PhotosDataSource dataSource;
 
-  final PhotosRemoteDataSource remoteDataSource;
-
-  PhotoRepositoryImpl({required this.remoteDataSource});
+  PhotoRepositoryImpl({required this.dataSource});
 
   @override
-  Future<ApiResult<Photos>> getRecentPhotos() {
-    // TODO: implement getRecentPhotos
-    throw UnimplementedError();
+  Future<ApiResult<Photos>> getRecentPhotos() async {
+    final result = await dataSource.getRecentPhotos();
+    return result.when(
+      success: (data) => ApiResult.success(data: data),
+      failure: (error) => ApiResult.failure(error: error),
+    );
   }
 
   @override
-  Future<ApiResult<Photos>> searchPhotos({required SearchCriteria searchCriteria}) {
-    // TODO: implement searchPhotos
-    throw UnimplementedError();
+  Future<ApiResult<Photos>> searchPhotos(
+      {required SearchCriteria searchCriteria}) async {
+      final result =
+          await dataSource.searchPhotos(searchCriteria: searchCriteria);
+      return result.when(
+        success: (data) => ApiResult.success(data: data),
+        failure: (error) => ApiResult.failure(error: error),
+      );
   }
-
-
-
-
-
-
 }
